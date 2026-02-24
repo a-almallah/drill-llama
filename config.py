be7c6ff30337e22037d5dded53446daf7f535e39
a-almallah <abodi.almallah@gmail.com>
2026-02-24 11:34:02 +0400

    refactor to config file properly with ints

--- config.py
+++ config.py
@@ -5,3 +5,9 @@ load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 ALLOWED_USER_ID = os.getenv("ALLOWED_USER_ID")
+if ALLOWED_USER_ID:
+    ALLOWED_USER_ID = int(ALLOWED_USER_ID)
+OLLAMA_URL = os.getenv("OLLAMA_URL", "http://localhost:11434/api/generate")
+
+if not TELEGRAM_BOT_TOKEN or not ALLOWED_USER_ID:
+    print("Warning: Missing TELEGRAM_BOT_TOKEN or ALLOWED_USER_ID")
